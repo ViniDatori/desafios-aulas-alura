2ea5c95852933176118af857c392e7b14306d2f0
Vinícius Datori <vinidatori@gmail.com>
2025-08-30 21:43:59 -0300

    Feitos os desafios do último módulo.

---
+++ C2 (Funções e Listas JS)/C2 Módulo 4.js
@@ -0,0 +1,20 @@
+// 1.	Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+// 2.	Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+
+// 3.	Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push("Java", "Ruby", "GoLang");
+
+// 4.	Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let lista1 = ["Vinícius", "Aline", "Gustavo"];
+lista1[0];
+
+// 5.	Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let lista2 = ["Nilceia", "Everaldo", "Arthur"];
+lista2[1];
+
+// 6.	Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let lista3 = ["Dejanir", "Léia", "Mariane"];
+lista3[2];
